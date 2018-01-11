50fc69b83c29321f198277ade822e95e0ea88913
hoangthong1989 <34851692+hoangthong1989@users.noreply.github.com>
2018-01-10 23:45:07 -0500

    f2
    d

--- Firefox2.js
+++ Firefox2.js
@@ -4,7 +4,7 @@ iimPlayCode("TAB CLOSEALLOTHERS");
 iimPlayCode("URL GOTO=about:home");
 ClearCookie();
 
-var Ma_off = ["31","157","71","109","10","45"];
+var Ma_off = ["31","30","159","67","71","103"];
 var File_Mail_info = 2; var sl_chay_off = 5; var off = 0;
 while(true)
 {
@@ -95,12 +95,12 @@ while(true)
 				}
 				// Dailybreak
 				if(title == "What Coffee Are You?"||title == "Make 2018 Your Best Year with Great Tips and Big Savings"||title == "Have Yourself A Very Merry Holiday"||title == "Gorton's Guide to Holiday Hosting"||title == "Celebrate The Holidays With Happiness, And Snacks!"||title == "Celebrate the Flavor of the Holidays"||title == "Barilla's Passport To Wonderful At ShopRite"||title == "Dove Wants To Know: Are there any dynamic teenage girls in your life?"||title == "Celebrate The Holidays With Happiness, And Snacks!"||title == "What Meal Will Get Your Family To The Table Tonight?"||title == "Discover Your Perfect Holiday Pair"||title == "How Should You Celebrate Fall?"||title == "Getting Through The Holidays Germ Free With CVS Pharmacy"||title == "Because You Were Born To Move, Let's Move with Dr. Scholl's®"||title == "When Your Family Gathers, Things Get Messy. Ours Can Help."||title == "Enjoy The Drive"||title == "Start Strong With Mott's® On-The-Go 100% Juice Pouches"||title == "Deo Made Simple"||title == "Once Upon a Whole Grain"||title == "KNOW YOUR PROTEIN"||title == "#MYOREOCREATION #CONTEST"||title == "Despicably Delicious"||title == "Refresh & Revive Your Hair This Summer"||title == "Pick Your Pepper"||title == "They Grow Up Fast – See How You Can Savor Every Delicious Moment"||title == "Transform Your Laundry Routine into a Sensorial Experience")
-				{ // 47 57 11 20 27 66 12 67 8 77 79 104 127 157
+				{ // 47 57 11 20 27 66 12 67 8 77 79 104 127 157 159
 				    try
 					{
 						switch(Check_Off)
 						{
-							case "157":case "127":case "104":case "79":case "77":case "8":case "67":
+							case "159":case "157":case "127":case "104":case "79":case "77":case "8":case "67":
 							case "12":case "66":case "27":case "20":case "11":case "47":case "57":
 							Dailybreak(File_Mail_info);
 							break;
@@ -1399,7 +1399,7 @@ function Dailybreak(File_Mail_info)
 	var Error_capcha = 0;var Failed_Recaptcha = 0;var Unknown_error = 0;
 	switch(Check_Off)
 	{// Lay info Khac neu Lam Off Giong nhau
-		case "27":
+		case "67":
 		Line = 2;
 		break;
 	}
@@ -1457,7 +1457,7 @@ function Dailybreak(File_Mail_info)
 				{
 					// khao sat khong doi nhan net
 					case "157":case "127":case "104":case "79":case "77":case "8":case "67":
-					case "12":case "66":case "27":case "20":case "11":case "57":case "47":
+					case "159":case "12":case "66":case "27":case "20":case "11":case "57":case "47":
 					var Dailybreak_KS1 = "CODE:";
 					Dailybreak_KS1 += "SET !ERRORIGNORE YES"+"\n";
 					Dailybreak_KS1 += "SET !TIMEOUT_TAG 0 "+"\n";
