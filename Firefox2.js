e6edbd3fbd62c71639e0edeb774336d317a07180
hoangthong1989 <34851692+hoangthong1989@users.noreply.github.com>
2018-01-22 00:31:19 -0500

    f2
    d

--- Firefox2.js
+++ Firefox2.js
@@ -4,7 +4,7 @@ iimPlayCode("TAB CLOSEALLOTHERS");
 iimPlayCode("URL GOTO=about:home");
 ClearCookie();
 
-var Ma_off = ["31","119","239","175","233","203"];
+var Ma_off = ["245","258","239","173","181","203"];
 var File_Mail_info = 2; var sl_chay_off = 5; var off = 0;
 while(true)
 {
@@ -1822,7 +1822,7 @@ function Dailybreak(File_Mail_info)
 	// Lay info Khac neu Lam Off Giong nhau
 	switch(Check_Off)
 	{
-		case "193":
+		case "173":
 		Line = 2;
 		break;
 	}
