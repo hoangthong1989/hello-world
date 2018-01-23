330ace2008d43ff563a2a922d52dab800d5cbc66
hoangthong1989 <34851692+hoangthong1989@users.noreply.github.com>
2018-01-22 22:29:59 -0500

    f1
    d

--- Firefox1.js
+++ Firefox1.js
@@ -4,8 +4,8 @@ iimPlayCode("TAB CLOSEALLOTHERS");
 iimPlayCode("URL GOTO=about:home");
 ClearCookie();
 
-var Ma_off = ["291","175","239","258","240","233",""]; // 
-var File_Mail_info = 1; var sl_chay_off = 5; var off = 0;
+var Ma_off = ["245","129","239","240","233",""]; // 
+var File_Mail_info = 1; var sl_chay_off = 4; var off = 0;
 while(true)
 {
 	if(off > sl_chay_off)
@@ -98,12 +98,12 @@ while(true)
 				}
 				// Dailybreak
 				if(title == "What Coffee Are You?"||title == "Make 2018 Your Best Year with Great Tips and Big Savings"||title == "Have Yourself A Very Merry Holiday"||title == "Gorton's Guide to Holiday Hosting"||title == "Celebrate The Holidays With Happiness, And Snacks!"||title == "Celebrate the Flavor of the Holidays"||title == "Barilla's Passport To Wonderful At ShopRite"||title == "Dove Wants To Know: Are there any dynamic teenage girls in your life?"||title == "Celebrate The Holidays With Happiness, And Snacks!"||title == "What Meal Will Get Your Family To The Table Tonight?"||title == "Discover Your Perfect Holiday Pair"||title == "How Should You Celebrate Fall?"||title == "Getting Through The Holidays Germ Free With CVS Pharmacy"||title == "Because You Were Born To Move, Let's Move with Dr. Scholl's®"||title == "When Your Family Gathers, Things Get Messy. Ours Can Help."||title == "Enjoy The Drive"||title == "Start Strong With Mott's® On-The-Go 100% Juice Pouches"||title == "Deo Made Simple"||title == "Once Upon a Whole Grain"||title == "KNOW YOUR PROTEIN"||title == "#MYOREOCREATION #CONTEST"||title == "Despicably Delicious"||title == "Refresh & Revive Your Hair This Summer"||title == "Pick Your Pepper"||title == "They Grow Up Fast – See How You Can Savor Every Delicious Moment"||title == "Transform Your Laundry Routine into a Sensorial Experience")
-				{ // 47 57 11 20 27 66 12 67 8 77 79 104 127 157 159 173 177 175 182 193 251 258 277 195
+				{ // 47 57 11 20 27 66 12 67 8 77 79 104 127 157 159 173 177 175 182 193 251 258 277 195 129
 				    try
 					{
 						switch(Check_Off)
 						{
-							case "195":case "277":case "258":case "251":case "193":case "182":
+							case "129":case "195":case "277":case "258":case "251":case "193":case "182":
 							case "175":case "159":case "157":case "127":case "104":case "79":case "77":case "8":case "67":
 							case "173":case "177":case "12":case "66":case "27":case "20":case "11":case "47":case "57":
 							Dailybreak(File_Mail_info);
@@ -1891,7 +1891,7 @@ function Dailybreak(File_Mail_info)
 			{// Khao Sat DAILY
 				switch(Check_Off)
 				{
-					case "195":case "277":case "258":case "251":case "193":case "178":case "182":
+					case "129":case "195":case "277":case "258":case "251":case "193":case "178":case "182":
 					case "177":case "173":case "157":case "127":case "104":case "79":case "77":case "8":case "67":
 					case "175":case "159":case "12":case "66":case "27":case "20":case "11":case "57":case "47":
 					var Dailybreak_KS1 = "CODE:";
