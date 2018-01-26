c55137223e4ce1de68aa03a28eab12c2e4105935
hoangthong1989 <34851692+hoangthong1989@users.noreply.github.com>
2018-01-26 03:00:51 -0500

    f1
    d

--- Firefox1.js
+++ Firefox1.js
@@ -8,9 +8,9 @@ home += "URL GOTO=about:home"+"\n";
 iimPlay(home);
 ClearCookie();
 
-var Ma_off = ["318","299","245","297","341","313","319"];
-// var Ma_off = ["315","299","330","297","341","315","319"];
-var File_Mail_info = 1; var sl_chay_off = 6; var off = 0; // Samples Avenue : Beauty
+var Ma_off = ["305","86","177","343","316","319"];
+// var Ma_off = ["305","84","177","343","316","319"];
+var File_Mail_info = 1; var sl_chay_off = 6; var off = 0;
 while(true)
 {
 	if(off > sl_chay_off)
@@ -104,11 +104,12 @@ while(true)
 				}
 				// Dailybreak
 				if(title == "Game Day Is Here! Are You Prepared?"||title == "What Coffee Are You?"||title == "Make 2018 Your Best Year with Great Tips and Big Savings"||title == "Have Yourself A Very Merry Holiday"||title == "Gorton's Guide to Holiday Hosting"||title == "Celebrate The Holidays With Happiness, And Snacks!"||title == "Celebrate the Flavor of the Holidays"||title == "Barilla's Passport To Wonderful At ShopRite"||title == "Dove Wants To Know: Are there any dynamic teenage girls in your life?"||title == "Celebrate The Holidays With Happiness, And Snacks!"||title == "What Meal Will Get Your Family To The Table Tonight?"||title == "Discover Your Perfect Holiday Pair"||title == "How Should You Celebrate Fall?"||title == "Getting Through The Holidays Germ Free With CVS Pharmacy"||title == "Because You Were Born To Move, Let's Move with Dr. Scholl's®"||title == "When Your Family Gathers, Things Get Messy. Ours Can Help."||title == "Enjoy The Drive"||title == "Start Strong With Mott's® On-The-Go 100% Juice Pouches"||title == "Deo Made Simple"||title == "Once Upon a Whole Grain"||title == "KNOW YOUR PROTEIN"||title == "#MYOREOCREATION #CONTEST"||title == "Despicably Delicious"||title == "Refresh & Revive Your Hair This Summer"||title == "Pick Your Pepper"||title == "They Grow Up Fast – See How You Can Savor Every Delicious Moment"||title == "Transform Your Laundry Routine into a Sensorial Experience")
-				{ // 47 57 11 20 27 66 12 67 8 77 79 104 127 157 159 173 177 175 182 193 251 258 277 195 129 297 341
+				{ // 47 57 11 20 27 66 12 67 8 77 79 104 127 157 159 173 177 175 182 193 251 258 277 195 129 297 341 343
 				    try
 					{
 						switch(Check_Off)
 						{
+							case "343":
 							case "341":case "297":case "129":case "195":case "277":case "258":case "251":case "193":
 							case "175":case "159":case "157":case "127":case "104":case "173":case "177":case "182":
 							case "79":case "77":case "67":case "12":case "66":case "27":case "20":case "11":case "47":
@@ -1967,7 +1968,7 @@ function Dailybreak(File_Mail_info)                      // Dailybreak
 	// Lay info Khac neu Lam Off Giong nhau
 	switch(Check_Off)
 	{
-		case "341":
+		case "177":
 		Line = 2;
 		break;
 	}
@@ -2023,6 +2024,7 @@ function Dailybreak(File_Mail_info)                      // Dailybreak
 			{// Khao Sat DAILY
 				switch(Check_Off)
 				{
+					case "343":
 					case "297":case "129":case "195":case "277":case "258":case "251":case "193":case "178":case "182":
 					case "177":case "173":case "157":case "127":case "104":case "79":case "77":case "8":case "67":
 					case "175":case "159":case "12":case "66":case "27":case "20":case "11":case "57":case "47":
@@ -2520,7 +2522,7 @@ function Depend(File_Mail_info)
 	Depend += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:fsForm2673984 ATTR=ID:field51877497-last CONTENT={{!COL2}}"+"\n";
 	Depend += "SET !DATASOURCE Info_FF"+File_Mail_info+".txt"+"\n";
 	Depend += "SET !DATASOURCE_LINE 1"+"\n";
-	Depend += "SET !DATASOURCE_COLUMNS 5 | "+"\n";
+	Depend += "SET !DATASOURCE_COLUMNS 5"+"\n";
 	Depend += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:fsForm2673984 ATTR=ID:field51877498 CONTENT={{!COL2}}"+"\n";
 	Depend += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:fsForm2673984 ATTR=ID:field51877500 CONTENT={{!COL3}}"+"\n";
 	Depend += "TAG POS=1 TYPE=SELECT FORM=ID:fsForm2673984 ATTR=ID:field51877501 CONTENT=%{{!COL4}}"+"\n";
