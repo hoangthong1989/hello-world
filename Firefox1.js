56bd498c16ec65a92219e6ca071a3dfeb6dfb92b
hoangthong1989 <34851692+hoangthong1989@users.noreply.github.com>
2018-01-26 20:04:59 -0500

    f1
    d

--- Firefox1.js
+++ Firefox1.js
@@ -8,8 +8,8 @@ home += "URL GOTO=about:home"+"\n";
 iimPlay(home);
 ClearCookie();
 
-var Ma_off = ["174","345","175","297","319","281"];
-var File_Mail_info = 1; var sl_chay_off = 5; var off = 0;
+var Ma_off = ["174","175","297","319","281"];
+var File_Mail_info = 1; var sl_chay_off = 4; var off = 0;
 while(true)
 {
 	if(off > sl_chay_off)
