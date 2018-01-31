a37393f7cfccf34214299a4cba4910c74c8109f3
hoangthong1989 <34851692+hoangthong1989@users.noreply.github.com>
2018-01-31 01:23:07 -0500

    f2
    d

--- Firefox2.js
+++ Firefox2.js
@@ -7,7 +7,7 @@ home += "TAB CLOSEALLOTHERS"+"\n";
 home += "URL GOTO=about:home"+"\n";
 iimPlay(home);ClearCookie();
 
-var Ma_off = ["305","341","127","320","352","319","245"];
+var Ma_off = ["305","341","376","320","352","319","245"];
 // var Ma_off = ["363"];
 var File_Mail_info = 2; var sl_chay_off = 6; var off = 0; 
 while(true)
@@ -40,7 +40,7 @@ while(true)
 		////////////// Cho Lệnh Chờ Nếu Site Bị Trắng /////////////////// 
 		switch(Check_Off)
 		{
-			case "345":
+			case "376":case "345":
 			case "310":case "305":case "175":case "297":case "292":case "291":case "286":case "277":
 			case "181":case "176":case "171":case "110":case "252":case "203":case "172":case "187":
 			case "61":case "26":case "10":
@@ -103,12 +103,12 @@ while(true)
 					break;
 				}
 				if(title == "Game Day Is Here! Are You Prepared?"||title == "What Coffee Are You?"||title == "Make 2018 Your Best Year with Great Tips and Big Savings"||title == "Have Yourself A Very Merry Holiday"||title == "Gorton's Guide to Holiday Hosting"||title == "Celebrate The Holidays With Happiness, And Snacks!"||title == "Celebrate the Flavor of the Holidays"||title == "Barilla's Passport To Wonderful At ShopRite"||title == "Dove Wants To Know: Are there any dynamic teenage girls in your life?"||title == "Celebrate The Holidays With Happiness, And Snacks!"||title == "What Meal Will Get Your Family To The Table Tonight?"||title == "Discover Your Perfect Holiday Pair"||title == "How Should You Celebrate Fall?"||title == "Getting Through The Holidays Germ Free With CVS Pharmacy"||title == "Because You Were Born To Move, Let's Move with Dr. Scholl's®"||title == "When Your Family Gathers, Things Get Messy. Ours Can Help."||title == "Enjoy The Drive"||title == "Start Strong With Mott's® On-The-Go 100% Juice Pouches"||title == "Deo Made Simple"||title == "Once Upon a Whole Grain"||title == "KNOW YOUR PROTEIN"||title == "#MYOREOCREATION #CONTEST"||title == "Despicably Delicious"||title == "Refresh & Revive Your Hair This Summer"||title == "Pick Your Pepper"||title == "They Grow Up Fast – See How You Can Savor Every Delicious Moment"||title == "Transform Your Laundry Routine into a Sensorial Experience")
-				{ // 104 127 157 159 173 177 175 182 193 251 258 277 195 129 297 341 343 361 363
+				{ // 104 127 157 159 173 177 175 182 193 251 258 277 195 129 297 341 343 361 363 376
 				    try
 					{
 						switch(Check_Off)
 						{
-							case "363":case "361":case "343":
+							case "376":case "363":case "361":case "343":
 							case "341":case "297":case "129":case "195":case "277":case "258":case "251":case "193":
 							case "175":case "159":case "157":case "127":case "104":case "173":case "177":case "182":
 							case "79":case "77":case "67":case "12":case "66":case "27":case "20":case "11":case "47":
@@ -1793,7 +1793,7 @@ function Dailybreak(File_Mail_info)                      // Dailybreak
 	// Lay info Khac neu Lam Off Giong nhau
 	switch(Check_Off)
 	{
-		case "127":
+		case "376":
 		Line = 2;
 		break;
 	}
@@ -1849,7 +1849,7 @@ function Dailybreak(File_Mail_info)                      // Dailybreak
 			{// Neu Ok Thi Chay KS
 				switch(Check_Off)
 				{// KS loại 1
-					case "363":case "343":
+					case "376":case "363":case "343":
 					case "297":case "129":case "195":case "277":case "258":case "251":case "193":case "178":case "182":
 					case "177":case "173":case "157":case "127":case "104":case "79":case "77":case "8":case "67":
 					case "175":case "159":case "12":case "66":case "27":case "20":case "11":case "57":case "47":
